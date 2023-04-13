7bc7875c88a67c54b7bc6f68481010b78ae81179
jxbbb <jinbu18@mails.ucas.ac.cn>
2023-04-13 08:12:56 +0000

    fix some conflicts that may happen in different experiment settings

--- launch_container.sh
+++ launch_container.sh
@@ -1,5 +1,6 @@
 export REPO_DIR=$PWD
 DATA_DIR=$REPO_DIR'/datasets/'
+DATA_PART_DIR=$REPO_DIR'/datasets_part/'
 MODEL_DIR=$REPO_DIR'/models/'
 OUTPUT=$REPO_DIR'/output/'
 
@@ -17,6 +18,7 @@ fi
 docker run --gpus '"'device=$CUDA_VISIBLE_DEVICES'"' --ipc=host --rm -it \
     --mount src=$(pwd),dst=/videocap,type=bind \
     --mount src=$DATA_DIR,dst=/videocap/datasets,type=bind$RO \
+    --mount src=$DATA_PART_DIR,dst=/videocap/datasets_part,type=bind$RO \
     --mount src=$MODEL_DIR,dst=/videocap/models,type=bind,readonly \
     --mount src=$OUTPUT,dst=/videocap/output,type=bind \
     -e NVIDIA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES \
--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,7 +1,7 @@
 # After launching the docker container 
 EVAL_DIR='checkpoints/basemodel/checkpoints/'
 CHECKPOINT='checkpoints/basemodel/checkpoints/model.bin'
-VIDEO='/videocap/demo/1f693cf9-b3c730a4.mov'
+VIDEO='/videocap/demo/053da4e3-48ec49ba.mov'
 CUDA_VISIBLE_DEVICES=5 python -m pdb /videocap/src/tasks/run_caption_VidSwinBert_inference.py \
        --resume_checkpoint $CHECKPOINT  \
        --eval_model_dir $EVAL_DIR \
